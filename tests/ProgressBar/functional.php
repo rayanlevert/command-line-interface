60edfffb502351494e4eece0cd3c4d5e368606cb
Rayan <rlevert@group-dis.com>
2023-02-16 10:26:24 +0100

    DisDev\Cli\ProgressBar : ajout de la première version de la barre de progrès (->start() avec un titre et ->advance() à chaque iteration) #3

---
+++ tests/ProgressBar/functional.php
@@ -0,0 +1,68 @@
+<?php
+
+use DisDev\Cli\ProgressBar;
+
+print "\nProgress bar - max 10 step 1\n";
+
+$oProgressBar = new ProgressBar(10);
+$oProgressBar->start();
+
+foreach (range(1, 10) as $step) {
+    $oProgressBar->advance(1);
+
+    echo ob_get_clean();
+
+    usleep(200000);
+}
+
+print "\n\nProgress bar - max 10 step 1 with title\n";
+
+$oProgressBar = new ProgressBar(10);
+$oProgressBar->start('Barre de progrès');
+
+foreach (range(1, 10, 2) as $step) {
+    $oProgressBar->advance(2);
+
+    echo ob_get_clean();
+
+    usleep(400000);
+}
+
+print "\n\nProgress bar - max 10 step 2\n";
+
+$oProgressBar = new ProgressBar(10);
+$oProgressBar->start();
+
+foreach (range(1, 10, 2) as $step) {
+    $oProgressBar->advance(2);
+
+    echo ob_get_clean();
+
+    usleep(400000);
+}
+
+print "\n\nProgress bar - max 10 step 3\n";
+
+$oProgressBar = new ProgressBar(10);
+$oProgressBar->start();
+
+foreach (range(1, 12, 3) as $step) {
+    $oProgressBar->advance(3);
+
+    echo ob_get_clean();
+
+    usleep(400000);
+}
+
+print "\n\nProgress bar - max 100 step 1\n";
+
+$oProgressBar = new ProgressBar(100);
+$oProgressBar->start();
+
+foreach (range(1, 100) as $step) {
+    $oProgressBar->advance(1);
+
+    echo ob_get_clean();
+
+    usleep(50000);
+}
